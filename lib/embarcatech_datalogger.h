5d597fbf8b7fbdb531a7ecc600b5296ed924ec15
Camila <camila@example.com>
2025-07-29 23:18:49 -0300

    Enhance documentation and clean up code structure in embarcatech_datalogger.h

--- lib/embarcatech_datalogger.h
+++ lib/embarcatech_datalogger.h
@@ -21,32 +21,81 @@
 
 /* --------- VARIAVEIS E CONSTANTES --------- */
 
-#define DEBOUNCE_TIME 200 // Debounce time in milliseconds
+#define DEBOUNCE_TIME 200 // Debounce time em milisegundos
 #define MAX_NUM_SAMPLES 100
-extern const uint32_t LED_MASK;
-extern const uint32_t COLOR_RED;
-extern const uint32_t COLOR_GREEN;
-extern const uint32_t COLOR_BLUE;
-extern const uint32_t COLOR_WHITE;
-extern const uint32_t COLOR_YELLOW;
-extern const uint32_t COLOR_PURPLE;
+extern const uint32_t LED_MASK;     // Mascara para os LEDs
+extern const uint32_t COLOR_RED;    // Mascara para a cor vermelha
+extern const uint32_t COLOR_GREEN;  // Mascara para a cor verde
+extern const uint32_t COLOR_BLUE;   // Mascara para a cor azul
+extern const uint32_t COLOR_WHITE;  // Mascara para a cor branca
+extern const uint32_t COLOR_YELLOW; // Mascara para a cor amarela
+extern const uint32_t COLOR_PURPLE; // Mascara para a cor roxa
 
 /* --------- STRUCTS E ENUMS --------- */
 
+
+
+/**
+ * @enum System_state_t
+ * @brief Representa os vários estados do sistema.
+ *
+ * Esta enumeração define os possíveis estados operacionais do sistema:
+ * - SYSTEM_STATE_IDLE: O sistema está ocioso e não executa operações.
+ * - SYSTEM_STATE_MOUNTING: O sistema está montando o microSD.
+ * - SYSTEM_STATE_UNMOUNTING: O sistema está desmontando o microSD.
+ * - SYSTEM_STATE_READING: O sistema está lendo dados ativamente.
+ */
 typedef enum {
     SYSTEM_STATE_IDLE,
     SYSTEM_STATE_MOUNTING,
     SYSTEM_STATE_UNMOUNTING,
-    SYSTEM_STATE_FORMATTING,
-    SYSTEM_STATE_READING,
-    SYSTEM_STATE_WRITING
+    SYSTEM_STATE_READING
 } System_state_t;
 
 /* --------- PROTOTIPOS DE FUNCOES --------- */
 
+/**
+ * @brief Manipulador de interrupção para eventos de GPIO.
+ * 
+ * Esta função é chamada quando ocorre um evento de interrupção em um pino GPIO específico.
+ * 
+ * @param gpio Número do pino GPIO que gerou a interrupção.
+ * @param events Máscara de eventos que ocorreram no pino.
+ */
+
 void gpio_irq_handler(uint gpio, uint32_t events);
+
+/**
+ * @brief Limpa a tela e desenha uma string na tela nas coordenadas especificadas.
+ * 
+ * @param str Ponteiro para a string a ser desenhada.
+ * @param x Posição horizontal onde a string será desenhada.
+ * @param y Posição vertical onde a string será desenhada.
+ */
 static void draw_string(const char *str, uint8_t x, uint8_t y);
+
+/**
+ * @brief Desenha a interface de usuário em estado ocioso.
+ * 
+ * Esta função é responsável por exibir a interface gráfica quando o sistema está em modo idle.
+ */
+
 static void draw_idle_interface();
+/**
+ * @brief Desenha a interface de captura de dados.
+ * 
+ * Esta função exibe a interface gráfica utilizada durante o processo de captura de dados.
+ */
+
 static void draw_capture_interface();
+/**
+ * @brief Realiza a captura de dados e salva os resultados.
+ * 
+ * Esta função executa a aquisição dos dados necessários e realiza o salvamento dos mesmos.
+ * 
+ * @return Retorna 0 em caso de sucesso ou um valor negativo em caso de erro.
+ */
 static int capture_data_and_save();
+
+
 #endif // EMBARCA_TECH_DATALOGGER_H
